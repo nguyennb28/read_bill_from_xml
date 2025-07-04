6c426b52e82f96467de7fe6ac3ca335e14fd54fa
nguyennb28 <nguyennb.coding@gmail.com>
2025-07-04 12:08:25 +0700

    upload 2 file final (get invoice_names)

--- final2.py
+++ final2.py
@@ -2,13 +2,6 @@ import xml.etree.ElementTree as ET
 import os
 
 namespace = {"inv": "http://laphoadon.gdt.gov.vn/2014/09/invoicexml/v1"}
-"""
-    Output: [{
-        "SHDon": value,
-        "Total": value,   
-    }]
-"""
-
 results = []
 
 for filename in os.listdir():
@@ -16,43 +9,33 @@ for filename in os.listdir():
         tree = ET.parse(filename)
         root = tree.getroot()
 
-        # INVOICE NAME
-        invoice_names = [elem.text for elem in root.findall(".//THHDVu") if elem.text]
-        invoice_names += [
-            elem.text
-            for elem in root.findall(".//inv:itemName", namespace)
-            if elem.text
-        ]
+        # Lấy tất cả itemName (cả có namespace và không)
+        invoice_names = [elem.text for elem in root.findall(".//inv:itemName", namespace) if elem.text]
+        invoice_names += [elem.text for elem in root.findall(".//THHDVu") if elem.text]
 
-        # INVOICE ELEMENT
+        # Lấy shdon_value
         invoice_number_elem = root.find(".//inv:invoiceNumber", namespace)
-        shdon_value = (
-            invoice_number_elem.text if invoice_number_elem is not None else None
-        )
-
+        shdon_value = invoice_number_elem.text if invoice_number_elem is not None else None
         if not shdon_value:
             for elem in root.iter():
                 if elem.tag.startswith("SHDon") and elem.text:
                     shdon_value = elem.text
                     break
 
-        # INVOICE TOTAL
+        # Lấy total_value
         invoice_total_elem = root.find(".//inv:totalAmountWithVAT", namespace)
-        total_value = (
-            invoice_total_elem.text if invoice_total_elem is not None else None
-        )
-
+        total_value = invoice_total_elem.text if invoice_total_elem is not None else None
         if not total_value:
             for elem in root.iter():
                 if elem.tag.startswith("TgTTTBSo") and elem.text:
                     total_value = elem.text
                     break
 
+        # Chỉ thêm vào kết quả nếu có đủ thông tin
         if shdon_value and total_value:
             results.append(
                 {"itemName": invoice_names, "shdon": shdon_value, "total": total_value}
             )
-
 
 for x in results:
     print(x)
